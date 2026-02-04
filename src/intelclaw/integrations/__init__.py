436eef95b95dbb21d9c1797da055f2c926e75ccc
MR-PROFESSOR-790 <231360@students.au.edu.pk>
2026-02-04 18:07:37 +0500

    feat: Add GitHubAuth to integrations module exports

--- src/intelclaw/integrations/__init__.py
+++ src/intelclaw/integrations/__init__.py
@@ -1,6 +1,6 @@
 """Integrations module - External service integrations."""
 
 from intelclaw.integrations.copilot import CopilotIntegration, ModelManager
-from intelclaw.integrations.llm_provider import LLMProvider, CopilotLLM
+from intelclaw.integrations.llm_provider import LLMProvider, CopilotLLM, GitHubAuth
 
-__all__ = ["CopilotIntegration", "ModelManager", "LLMProvider", "CopilotLLM"]
+__all__ = ["CopilotIntegration", "ModelManager", "LLMProvider", "CopilotLLM", "GitHubAuth"]
